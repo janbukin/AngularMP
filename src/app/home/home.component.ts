35786bb9265f193a4330f5d6cd067f12c8520073
Jan Bukin <jdbukin@gmail.com>
2017-11-27 19:48:53 +0300

    Added init structure with Webpack and Angular

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import { AppState } from '../app.service';
+
+@Component({
+  selector: 'home',  // <home></home>
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  /**
+   * Set our default values
+   */
+  public localState = { value: '' };
+  /**
+   * TypeScript public modifiers
+   */
+  constructor(
+    public appState: AppState,
+  ) {}
+
+  public ngOnInit() {
+    console.log('hello `Home` component');
+    /**
+     * this.title.getData().subscribe(data => this.data = data);
+     */
+  }
+
+  public submitState(value: string) {
+    console.log('submitState', value);
+    this.appState.set('value', value);
+    this.localState.value = '';
+  }
+}
